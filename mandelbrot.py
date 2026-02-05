dabba8f323d226f136a6176bc077ee85e13ee448
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2026-02-05 15:00:22 +0100

    mandelbrotpoint changes

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,17 +3,25 @@ Mandelbrot Set Generator
 Author : [ Mathias Jørgensen ]
 Course : Numerical Scientific Computing 2026
 """
-def f ( x ) :
-    """
-    Example function .
-    Parameters
-    ----------
-    x : float
-        Input value
-    Returns
-    -------
-    float
-        Output value
-    """
-    #TODO : Implement the algorithm
-    pass
+import numpy as np
+import matplotlib as plt
+
+
+def mandelbrotPoint(c):
+
+    # Parameters
+    z = 0
+    max_iter = 100
+
+    for n in range(max_iter):
+        z = z**2 + c 
+
+        if abs(z) > 2:
+            return n
+    return max_iter
+
+if __name__ == "__main__":
+    c = 1 + 1j
+    print(c)
+    mandelbrotPoint(c)
+
