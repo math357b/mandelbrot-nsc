ea5c10a75d865a822707f2f63d5ceb7c75d33f2c
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2026-02-05 15:33:19 +0100

    added compute_mandelbrot

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,8 +6,7 @@ Course : Numerical Scientific Computing 2026
 import numpy as np
 import matplotlib as plt
 
-
-def mandelbrotPoint(c):
+def mandelbrot_point(c):
 
     # Parameters
     z = 0
@@ -16,10 +15,24 @@ def mandelbrotPoint(c):
     for n in range(max_iter):
         z = z**2 + c 
         if abs(z) > 2:
-            return n
+            return n 
+        
+def compute_mandelbrot(x_min, x_max, y_min, y_max, resx, resy):
+    
+    x = np.linspace(x_min, x_max, resx)
+    y = np.linspace(y_min, y_max, resy)
+
+    #create array for c
+    all_c = np.zeros(resx, resy, dtype=complex)
+    all_n = np.zeros(resx, resy, dtype=int)  
+
+    for i in range(resx):
+        for j in range(resy):
+            all_c[i,j] = x[i] + 1j * y[j]
+            all_n[i,j] = mandelbrot_point(all_c[i,j])
+    return all_c, all_n
 
 if __name__ == "__main__":
-    c = 1 + 1j
-    n = mandelbrotPoint(c)
-    print(f'complex number is: {=c}, number of iterations: {=n}')
-
+    all_c, all_n = compute_mandelbrot(-2, 1, -1.5, 1.5, 100, 100)
+    print(all_c)
+    print(all_n)
