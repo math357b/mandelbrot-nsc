d0f3572657a4c9e523818217a8d21a39c046cba1
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2026-03-05 15:16:49 +0100

    Adds Monte Carlo Exercise 2

--- monte_carlo_example.py
+++ monte_carlo_example.py
@@ -1,22 +1,39 @@
+from multiprocessing import Pool
+import os
 import math, random, time, statistics
 
-def estimate_pi_serial(num_samples):
+def estimate_pi_serial(num_samples: int):
     inside_cirle = 0
     for _ in range(num_samples):
         x, y = random.random(), random.random() # get random number for x and y
         if x**2 + y**2 <= 1:
             inside_cirle += 1
     pi_serial = 4 * inside_cirle / num_samples
-
     return pi_serial
+
+def estimate_pi_chunk(num_samples: int):
+    inside_cirle = 0
+    for _ in range(num_samples):
+        x, y = random.random(), random.random() # get random number for x and y
+        if x**2 + y**2 <= 1:
+            inside_cirle += 1
+    return inside_cirle
+
+def estimate_pi_parallel(num_samples: int,
+                         num_processes: int = 4):
+    samples_per_process = num_samples // num_processes
+    tasks = [samples_per_process] * num_processes
+    with Pool(processes=num_processes) as pool:
+        results = pool.map(estimate_pi_chunk, tasks)
+    return 4 * sum(results) / num_samples
 
 if __name__ == '__main__':
     num_samples = 10_000_000
-    times = []
-    for _ in range(3):
-        t0 = time.perf_counter()
-        pi_estimate = estimate_pi_serial(num_samples=num_samples)
-        times.append(time.perf_counter() - t0)
-    t_serial = statistics.median(times)
-    print(f"pi estimate: {pi_estimate:.6f} (error: {abs(pi_estimate - math.pi)})")
-    print(f"Serial time: {t_serial:.3f}")
+    for num_proc in range(1, os.cpu_count() + 1):
+        times = []
+        for _ in range(3):
+            t0 = time.perf_counter()
+            pi_estimate = estimate_pi_parallel(num_samples=num_samples, num_processes=num_proc)
+            times.append(time.perf_counter() - t0)
+        t_parallel = statistics.median(times)
+        print(f"{num_proc:2d} workers: {t_parallel:.3f}s, pi={pi_estimate:.6f}")
