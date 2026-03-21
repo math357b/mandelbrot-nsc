7408d98edf567d375cd0d0e0397f65edfb87d14c
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2026-03-21 18:34:28 +0100

    adds mandelbrot for L6 and computed Milestone 1 of lecture 6

---
+++ mandelbrot_6.py
@@ -0,0 +1,24 @@
+from dask import delayed
+from dask.distributed import Client, LocalCluster
+import dask, time, statistics
+import numpy as np
+from mandelbrot_5 import mandelbrot_chunk
+
+def mandelbrot_dask(N: int,
+                    x_dim: tuple[float, float],
+                    y_dim: tuple[float, float],
+                    max_iter: int = 100,
+                    n_chunks: int = 32):
+    
+    chunk_size = max(1, N // n_chunks)
+    tasks = []
+    row = 0
+
+    while row < N:
+        row_end = min(row + chunk_size, N)
+        tasks.append(delayed(mandelbrot_chunk)(row, row_end, N, x_dim, y_dim, max_iter))
+        row = row_end
+    
+    parts = dask.compute(*tasks)
+
+    return np.vstack(parts)
